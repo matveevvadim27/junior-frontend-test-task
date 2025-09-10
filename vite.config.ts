7ee95d277c4cf50ec286486b12677a7a3941538f
Vadim Matveev <matveev.vadim27@gmail.com>
2025-09-10 20:25:27 +0300

    chore: add svg config for using like react component

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,13 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
-import path from "path"
+import svgr from "vite-plugin-svgr"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@pages": path.resolve(__dirname, "src/pages")
+      "@": new URL("./src", import.meta.url).pathname
     }
   }
-  // css: {
-  //   preprocessorOptions: {
-  //     scss: {
-  //       additionalData: `@import "@/styles/helpers";`
-  //     }
-  //   }
-  // }
 })
